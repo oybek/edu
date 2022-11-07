2fe34c4e2e68ed486c6cf94b5b1d299fb7e41a36
oybek <o.khashimov@efg.gg>
2022-11-08 04:38:59 +0500

    working with tuples

--- ts/understanding-ts/app.ts
+++ ts/understanding-ts/app.ts
@@ -1,9 +1,20 @@
 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Oybek",
   age: 27,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+person.role = [0, 'admin'];
 
 let favouriteActivities: string[];
 favouriteActivities = ['Sports'];
