fe6ffa3eccab8aac46913da90ad1e05395588ebe
oybek <o.khashimov@efg.gg>
2022-11-08 03:47:53 +0500

    working with arrays

--- ts/understanding-ts/app.ts
+++ ts/understanding-ts/app.ts
@@ -1,10 +1,15 @@
 
-const person: {
-  name: string;
-  age: number;
-} = {
+const person = {
   name: "Oybek",
-  age: 27
+  age: 27,
+  hobbies: ['Sports', 'Cooking']
 };
 
+let favouriteActivities: string[];
+favouriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
