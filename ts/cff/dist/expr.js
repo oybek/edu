10668d6a237a7e380b57e0a30dc0b10450c85e7f
oybek <o.khashimov@eslgaming.com>
2022-11-17 03:45:05 +0500

    simplify the solution

--- ts/cff/dist/expr.js
+++ ts/cff/dist/expr.js
@@ -16,30 +16,14 @@ class Frac {
     }
 }
 const div = { group: 'div' };
-function calc(ns) {
+function calc(ns, toT, combine) {
     return ns.reduce((stack, token) => {
         switch (token.group) {
             case "frac":
-                return [token, ...stack];
+                return [toT(token), ...stack];
             case "div":
                 const [f2, f1, ...tail] = stack;
-                return [f1.divide(f2), ...tail];
-        }
-    }, Array())[0];
-}
-function divides(s1, s2) {
-    return s2.includes('/')
-        ? `${s1}/(${s2})`
-        : `${s1}/${s2}`;
-}
-function draw(ns) {
-    return ns.reduce((stack, token) => {
-        switch (token.group) {
-            case "frac":
-                return [token.toString(), ...stack];
-            case "div":
-                const [f2, f1, ...tail] = stack;
-                return [divides(f1, f2), ...tail];
+                return [combine(f1, f2), ...tail];
         }
     }, Array())[0];
 }
@@ -47,14 +31,19 @@ function backtrack(tokens) {
     const fracCount = tokens.filter((x) => x.group === "frac").length;
     const divCount = tokens.filter((x) => x.group === "div").length;
     if (fracCount === 10 && divCount === 9) {
-        return calc(tokens).isEqual(7) ? [tokens] : [];
+        return calc(tokens, (x) => x, (x, y) => x.divide(y)).isEqual(7) ? [tokens] : [];
     }
     return (fracCount < 10 ? backtrack([...tokens, new Frac(fracCount + 1)]) : [])
         .concat(fracCount > divCount + 1 ? backtrack([...tokens, div]) : []);
 }
 const solutions = backtrack([]);
+function divides(s1, s2) {
+    return s2.includes('/')
+        ? `${s1}/(${s2})`
+        : `${s1}/${s2}`;
+}
 console.log(solutions.length);
 console.log(solutions
-    .map((x) => draw(x))
+    .map((x) => calc(x, (x) => x.toString(), divides))
     .sort((a, b) => a.length - b.length)
     .join("\n"));
--- ts/cff/src/expr.ts
+++ ts/cff/src/expr.ts
@@ -22,41 +22,25 @@ type Token = Frac | Div
 
 const div: Div = { group: 'div' };
 
-function calc(ns: Token[]): Frac {
+function calc<T>(ns: Token[],
+                 toT: (a: Frac) => T,
+                 combine: (a: T, b: T) => T): T {
   return ns.reduce((stack, token) => {
     switch(token.group) {
       case "frac":
-        return [token, ...stack];
+        return [toT(token), ...stack];
       case "div":
         const [f2, f1, ...tail] = stack
-        return [f1.divide(f2), ...tail];
+        return [combine(f1, f2), ...tail];
     }
-  }, Array<Frac>())[0];
-}
-
-function divides(s1: string, s2: string): string {
-  return s2.includes('/')
-    ? `${s1}/(${s2})`
-    : `${s1}/${s2}`;
-}
-
-function draw(ns: Token[]): string {
-  return ns.reduce((stack, token) => {
-    switch(token.group) {
-      case "frac":
-        return [token.toString(), ...stack];
-      case "div":
-        const [f2, f1, ...tail] = stack
-        return [divides(f1, f2), ...tail];
-    }
-  }, Array<string>())[0];
+  }, Array<T>())[0];
 }
 
 function backtrack(tokens: Token[]): Token[][] {
   const fracCount = tokens.filter((x) => x.group === "frac").length;
   const divCount = tokens.filter((x) => x.group === "div").length;
   if (fracCount === 10 && divCount === 9) {
-    return calc(tokens).isEqual(7) ? [tokens] : [];
+    return calc(tokens, (x) => x, (x, y) => x.divide(y)).isEqual(7) ? [tokens] : [];
   }
   return (fracCount < 10 ? backtrack([...tokens, new Frac(fracCount+1)]) : [])
   .concat(fracCount > divCount + 1 ? backtrack([...tokens, div]) : []);
@@ -64,10 +48,16 @@ function backtrack(tokens: Token[]): Token[][] {
 
 const solutions = backtrack([]);
 
+function divides(s1: string, s2: string): string {
+  return s2.includes('/')
+    ? `${s1}/(${s2})`
+    : `${s1}/${s2}`;
+}
+
 console.log(solutions.length);
 console.log(
 solutions
-  .map((x) => draw(x))
+  .map((x) => calc(x, (x) => x.toString(), divides))
   .sort((a, b) => a.length - b.length)
   .join("\n")
 );
